675aeb3a68981275583eecff00e93a9f950a9e54
AryanMandhare <mandharearyan123@gmail.com>
2025-07-03 19:45:49 +0530

    add day 2

---
+++ Day 2/script.js
@@ -0,0 +1,83 @@
+// alert("hello world");
+
+// Keyword(let,var,const)
+
+let name ="aryan";
+console.log(name);
+
+var address="wai";
+console.log(address);
+
+const age= 20;
+console.log(age);
+
+
+//  Data type in javascript
+
+// primitive data type
+
+// Number
+
+let height = 5.5;
+const PI = 3.14;
+ let RollNo = 10;
+
+console.log(typeof height);
+console.log(typeof PI);
+console.log(typeof RollNo);
+ 
+// String
+
+let userName ="aryan";
+let companyName = "mahindra";
+let collageName = "ATC.Bhor";
+
+console.log(typeof userName);
+console.log(typeof companyName);
+console.log(typeof collageName);
+
+// Boolean
+
+let IS_VALID_USER= true;
+let is_valid_account =false;
+let IS_Valid_Email =true;
+
+console.log(typeof IS_VALID_USER);
+console.log(typeof is_valid_account);
+console.log(typeof IS_Valid_Email);
+
+// Undefine
+
+let Bestcolleage
+let Address
+
+console.log(typeof Bestcolleage);
+console.log(typeof Address);
+
+// Bestcolleage="MIT.Pune"
+// console.log(typeof Bestcolleage);
+// console.log(Bestcolleage);
+
+// Null
+
+let abc= null;
+console.log(typeof abc);
+console.log(abc);
+
+let division = null;
+console.log(typeof division);
+console.log(division);
+
+// non-primitive data type
+
+// Object
+let car={};
+console.log(typeof car);
+
+// Array
+const arr =[];
+console.log(typeof arr);
+
+// Function
+let fun =()=>{};
+console.log(typeof fun);
