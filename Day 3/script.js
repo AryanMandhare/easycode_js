7b96da3f836b149a9d530722d2e7f9644e7914a1
AryanMandhare <mandharearyan123@gmail.com>
2025-07-04 16:25:57 +0530

    add day 3

---
+++ Day 3/script.js
@@ -0,0 +1,143 @@
+
+// 1] Arithmetic operator
+
+// + addition operator
+
+let first_number = 10;
+let second_number = 20;
+let results= first_number + second_number;
+console.log(results);
+
+console.log(20+30);
+
+// - subtraction operator
+  
+let A=30;
+let B=40;
+let C=A - B;
+console.log(C);
+
+console.log(70-30);
+
+// * multiplication operator
+
+let x=20;
+let y=23;
+let z= x * y;
+console.log(z);
+
+console.log(16 * 13);
+
+// / division operator
+
+let p=20;
+let q=5;
+let r= p/q;
+console.log(r);
+
+console.log(18/4);
+
+// % modulus operator
+
+let t= 9;
+let u= 2;
+let v= t % u;
+console.log(v);
+
+console.log(14 % 3);
+
+// ++ increment operator
+
+//pre-incremant
+let smn=1;
+console.log(++smn);
+console.log(smn);
+
+//post-increment
+let ram=0;
+console.log(ram++); 
+console.log(ram);
+
+
+
+// -- decrement operator
+
+//pre-decrement
+let asw=1;
+console.log(--asw);
+console.log(asw);
+
+//post-decrement
+let rst=0;
+console.log(rst--); 
+console.log(rst);
+
+// 2] Assignment operator
+
+let w=12;
+console.log(w);
+
+let a= 20;
+console.log(a);
+
+// += add assignment operator 
+
+let m = 30;
+   m= m+10;     // m+=10;
+console.log(m);
+
+// -= subtract assignment operator
+
+let s= 50;
+ s= s-20;    //s-=20;
+ console.log(s);
+
+ // *= multiplication assignment operator
+
+ let b= 50;
+  b= b*2;    // b*=2;
+  console.log(b);
+
+// /= division assignment operator
+
+let d = 30;
+ d= d/3;     // d/=3;
+  console.log(d);
+
+
+  // 3] Comparision operator
+ 
+  // == equal to operator
+
+  let f=10;
+  let g=10;
+  console.log(f==g);
+
+  let c=30;
+  let n='30';
+  console.log(c==n);
+
+  let first_name = "aryan";
+  let second_name = "Aryan";
+  console.log(first_name == second_name);
+
+     // === Strictly equal to operator
+  let H=10;
+  let J="10";    
+  console.log(H===J);  // it checks data type;
+
+  // != not equal to operator
+
+  let ghj=20;
+  let jkl=30;
+  console.log(ghj!=jkl);
+
+  let bnv=10;
+  let cvb='10';
+  console.log(bnv!=cvb);
+
+
+
+  let o=30;
+  let i=30;
+  console.log(o*i);
