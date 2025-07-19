97592129ed19c00fe81bd194c138c00508a0f20b
AryanMandhare <mandharearyan123@gmail.com>
2025-07-19 17:20:08 +0530

    pop up box

---
+++ Day 4/script.js
@@ -0,0 +1,20 @@
+// alert("invalid password");
+
+// prompt("enter your name:");
+
+// let userInput = prompt("Enter your name:");
+// console.log(userInput);
+
+// let firststring= prompt("Enter first number:");
+// firstnumber = parseInt(firststring);
+// let secondstring= prompt("Enter second number:");
+// secondnumber = parseInt(secondstring);
+// let sum=(firstnumber + secondnumber);
+// console.log(sum);
+
+let firstnum= prompt("Enter first number:");
+let firstnumber= Number(firstnum);
+let secondnum= prompt("Enter second number:");
+let secondnumber= Number(secondnum);
+let res=(firstnumber + secondnumber);
+console.log(res);
