647085a11a9874da7e8a646079a985bbd25b10fc
AryanMandhare <mandharearyan123@gmail.com>
2025-07-19 20:42:42 +0530

    conditional statement

---
+++ Day 5/script.js
@@ -0,0 +1,51 @@
+
+// if statement
+
+let age=20;
+
+ if(age >=18){
+        console.log("You are eligible to vote");
+       }
+
+let n=-5;
+       if(n < 0){
+              console.log(n,"is a negative number");
+       }
+
+
+
+ // if else statement
+
+let userinput= prompt("enter a number");
+ let number=parseInt(userinput);
+
+
+ if(number % 2 == 0){
+        console.log(number,"is even number");
+ }else{
+        console.log(number,"is odd number");
+
+ }
+
+
+ let i=20;
+ if(i % 2 == 0){
+        console.log(i,"is even number");
+ }else{
+        console.log(i,"is odd number");
+
+ }
+
+ let id= prompt("Enter your id");
+ if(id == "admin"){
+        console.log("Welcome admin");
+ }else{
+        console.log("Welcome user");
+ }
+
+ let num= prompt("enter a number:");
+ if(num >0){
+       console.log(num,"is a positive number");
+ }else{
+       console.log(num,"is a negative number");
+ }
