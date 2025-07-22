1ecfc42b165459c42353535dd83ba4ea787aa427
AryanMandhare <mandharearyan123@gmail.com>
2025-07-22 07:32:20 +0530

    document

---
+++ Day 9/script.js
@@ -0,0 +1,20 @@
+
+console.log(document.title="document in javascript");
+
+let mul_num= parseInt(prompt("Enter a number to multiply:"));
+document.write("<h1>table of " + mul_num + "</h1>");
+for(let i=1; i<=10; i++){
+    let result=mul_num*i;
+    document.write("<h3>"+result+"</h3>");
+}
+
+let userName="easycode";
+let websiteURL="https://easycode.support";
+
+console.log("hello "+ userName+" ,welcome to our website "+websiteURL);
+
+console.log(`hello ${userName} ,welcome to our website ${websiteURL}`)
+
+for(let i=0; i>=-20; i--){
+    console.log(`i is: ${i}`);
+}
