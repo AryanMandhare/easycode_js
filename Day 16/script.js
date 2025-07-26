3bbc5081e297e4e9b89217b58bdbe1a7958fd6d9
AryanMandhare <mandharearyan123@gmail.com>
2025-07-26 11:05:16 +0530

    learn document object module

---
+++ Day 16/script.js
@@ -0,0 +1,16 @@
+
+// get Element by tag name
+
+let result = document.getElementsByTagName("h1");
+console.log(result);
+
+let result1 = document.getElementsByTagName("h1");
+console.log(result1[1]);
+
+// get Element by class name
+
+let res = document.getElementsByClassName("container");
+console.log(res);
+
+let res1 = document.getElementsByClassName("container");
+console.log(res1[0]);
