d58438551dd0b23826f4af46eebd9a83a7a7c470
AryanMandhare <mandharearyan123@gmail.com>
2025-07-28 19:22:10 +0530

    mouse follower event

---
+++ Project/mouse follower/script.js
@@ -0,0 +1,7 @@
+
+let shadow = document.getElementById("moving-shadow");
+
+document.addEventListener("mousemove", (e) => {
+    shadow.style.left = `${e.offsetX}px`;
+    shadow.style.top = `${e.offsetY}px`;
+})
