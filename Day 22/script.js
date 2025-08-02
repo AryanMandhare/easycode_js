362af9820c8d12c7ebf92df538257dea5b0605d2
AryanMandhare <mandharearyan123@gmail.com>
2025-08-02 07:02:17 +0530

    JSON and local storage

--- Day 22/script.js
+++ Day 22/script.js
@@ -19,4 +19,44 @@ let obj = JSON.parse(str)
 console.log(typeof (obj))
 console.log(obj)
 
-localStorage.setItem("person", str)
+// 2] local storage
+// local storsge provide by the braower
+// Average capacity to Stores data only 5MB 
+// local storage store only primitive data types number, string,boolean
+
+// i] .setItem() - it used to add item into the local storage
+
+localStorage.setItem("user_name", "aryan")
+localStorage.setItem("is_login", true)
+localStorage.setItem("last_login", new Date())
+let person1 = {
+      name: "aryan",
+      age: 20
+}
+
+localStorage.setItem("user_info", JSON.stringify(person1))
+
+
+// ii] .getItem() - it used get/ read item to local storage
+
+let res1 = localStorage.getItem("user_name")
+console.log(res1)
+
+let res2 = localStorage.getItem("is_login")
+console.log(res2)
+
+let res3 = localStorage.getItem("last_login")
+console.log(res3)
+
+let res4 = JSON.parse(localStorage.getItem("user_info")) //  it convert string to object
+console.log(res4)
+
+// iii] .remoeItem() - it used to remove item to local storage
+
+localStorage.removeItem("user_name")
+localStorage.removeItem("is_login")
+
+// iv] .clear() - clear all data from local storage
+
+let res5 = localStorage.clear()
+console.log(res5)
