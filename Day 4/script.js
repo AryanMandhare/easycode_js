943abf293449d30728396e79bb0a7ac0a8c74846
AryanMandhare <mandharearyan123@gmail.com>
2025-08-02 07:18:34 +0530

    pop up box

--- Day 4/script.js
+++ Day 4/script.js
@@ -5,20 +5,20 @@ prompt("enter your name:");
 let userInput = prompt("Enter your name:");
 console.log(userInput);
 
-let firststring= prompt("Enter first number:");
-firstnumber = parseInt(firststring);
-let secondstring= prompt("Enter second number:");
-secondnumber = parseInt(secondstring);
-let sum=(firstnumber + secondnumber);
+let firststring = prompt("Enter first number:");
+let firstnumber = parseInt(firststring);
+let secondstring = prompt("Enter second number:");
+let secondnumber = parseInt(secondstring);
+let sum = (firstnumber + secondnumber);
 console.log(sum);
-                    
-            // OR
-let firstnum= prompt("Enter first number:");
-let firstnumber= Number(firstnum);
-let secondnum= prompt("Enter second number:");
-let secondnumber= Number(secondnum);
-let res=(firstnumber + secondnumber);
-console.log(res);
+
+           // OR
+// let firstnum= prompt("Enter first number:");
+// let firstnumber= Number(firstnum);
+// let secondnum= prompt("Enter second number:");
+// let secondnumber= Number(secondnum);
+// let res=(firstnumber + secondnumber);
+// console.log(res);
 
 
 confirm("are you leaving this page?");
